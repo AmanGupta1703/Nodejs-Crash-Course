bbea165acac6666db645214d370df479d2f9fbb6
Kunal Gupta <89504795+AmanGupta1703@users.noreply.github.com>
2023-10-13 19:33:40 +0530

    What is a os module??

---
+++ 03-OS-Module/app.js
@@ -0,0 +1,11 @@
+
+const os = require("os");
+
+console.log(os.freemem());      // free memory
+console.log(os.homedir());      // home directory
+console.log(os.hostname());     // hostname
+console.log(os.platform());     // platform
+console.log(os.release());      // release
+console.log(os.totalmem());    // total memory
+console.log(os.uptime());    // uptime
+console.log(os.type());    // type
